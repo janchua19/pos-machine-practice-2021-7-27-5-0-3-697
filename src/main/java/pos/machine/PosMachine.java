f44ff995f146cc720838085d1ee5072eccfd5ab9
janchua19 <jan.edward.chua@oocl.com>
2021-08-03 21:09:31 +0800

    [Fix3][JanEdwardChua]feat: Add computeTotalPrice Fuction

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -19,4 +19,12 @@ public class PosMachine {
         List<ItemInfo> itemsSubtotal = new ArrayList<>();
         return itemsSubtotal;
     }
+
+    public List<ItemInfo> computeTotalPrice(List<ItemInfo> itemsSubtotal)
+    {
+        List<ItemInfo> receipt = new ArrayList<>();
+        return receipt;
+    }
+
+
 }
